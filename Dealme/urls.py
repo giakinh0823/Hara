4b3b93b74324adad8e9a82e0cdd27abfc46a910c
Kính Hà Gia <giakinh2000@gmail.com>
2021-05-31 13:09:21 +0700

    social google login

--- Dealme/urls.py
+++ Dealme/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from allauth.account.views import LogoutView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,10 +23,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('Register.urls')),
+    path('', include('Register.urls')),
     path('', include('Home.urls')),
     path('', include('Product.urls')),
     path('', include('Order.urls')),
+    # google
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
