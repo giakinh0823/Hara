202d563b846c865927bf84c054eee090950c9a8e
Kính Hà Gia <giakinh2000@gmail.com>
2021-05-31 19:52:33 +0700

    nav menu responsive

--- static/components/Navbar/openNav.js
+++ static/components/Navbar/openNav.js
@@ -1,4 +1,3 @@
-
 const openNav = () => {
     document.querySelector('.nav-menu').style.display = "block"
 }
@@ -6,3 +5,28 @@ const openNav = () => {
 const closeNav = () => {
     setTimeout(() => document.querySelector('.nav-menu').style.display = "none", 400)
 }
+
+if (document.querySelector('.nav-menu-content-responsive').style.display === "flex") {
+        document.querySelector('#button-nav-menu-open').style.display = "none"
+        document.querySelector('#button-nav-menu-close').style.display = "block"
+    } else {
+        document.querySelector('#button-nav-menu-open').style.display = "block"
+        document.querySelector('#button-nav-menu-close').style.display = "none"
+        document.querySelector('.nav-menu-content-responsive')
+    }
+
+const openNavMenu = () => {
+    document.querySelector('.nav-menu-content-responsive').style.display = "flex"
+    document.querySelector('#button-nav-menu-open').style.display = "none"
+    document.querySelector('#button-nav-menu-close').style.display = "block"
+
+}
+
+const closeNavMenu = () => {
+    document.querySelector('.nav-menu-content-responsive').style.display = "none"
+    document.querySelector('#button-nav-menu-close').style.display = "block"
+    document.querySelector('#button-nav-menu-open').style.display = "block"
+    document.querySelector('#button-nav-menu-close').style.display = "none"
+}
+
+$('.body-container').mouseup(closeNavMenu)
