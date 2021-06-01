ecfc595dc0a2f7f24c3aa4bf703cccef127c5ee7
Kính Hà Gia <giakinh2000@gmail.com>
2021-06-01 09:43:10 +0700

    fix message

--- Messenger/urls.py
+++ Messenger/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "messenger"
 
 urlpatterns = [
-    path('messenger/<slug:slug>/', views.messenger, name="messenger")
+    path('messenger/<slug:slug>/', views.messenger, name="messenger"),
+    path('home_messenger/<slug:slug>/', views.home_messenger, name="home_messenger")
 ]
--- Messenger/views.py
+++ Messenger/views.py
@@ -7,27 +7,45 @@ from .models import *
 
 # Create your views here.
 
+@login_required
+def home_messenger(request, slug):
+    list_room = [*MessageRoom.objects.filter(person=request.user) , *MessageRoom.objects.filter(user=request.user)]
+    for room in list_room:
+        try:
+            person = Profile.objects.get(person=room.person)
+        except:
+            person = Profile.objects.get(user=room.user)
+        room.image = person.image
+        room.name = person.user.get_full_name()
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'Messenger/home_messenger.html',
+                  {'list_room': list_room, "profile": profile})
+
 
 @login_required
 def messenger(request, slug):
-    room = MessageRoom.objects.get(slug=slug)
-    list_room = MessageRoom.objects.filter(user = request.user)
+    try:
+        room = MessageRoom.objects.get(slug=slug)
+    except:
+        personSlug = str(slug)[len(request.user.username):]
+        person = User.objects.get(username=personSlug)
+        try:
+            room = MessageRoom.objects.get(user=request.user, person=person)
+        except:
+            room = MessageRoom.objects.create(user=request.user, person=person)
+            room = room.save()
+    list_room = [*MessageRoom.objects.filter(person=request.user) , *MessageRoom.objects.filter(user=request.user)]
     for room in list_room:
-        person = Profile.objects.get(user = room.person)
+        try:
+            person = Profile.objects.get(person=room.person)
+        except:
+            person = Profile.objects.get(user=room.user)
+        room.image = person.image
+        room.name = person.user.get_full_name()
         room.image = person.image
     messages = Message.objects.filter(room=room)
     profile = Profile.objects.get(user=request.user)
     for message in messages:
         message.is_date = message.created_at.strftime("%M:%S")
-    return render(request, 'Messenger/messenger.html', {"room": room, "messages": messages, 'list_room': list_room, "profile": profile})
-
-
-def create_room_message(request, slug):
-    try:
-        message = Message.objects.get(slug=slug)
-    except:
-        personSlug = str(slug)[len(request.user.username):]
-        person = User.objects.get(username=personSlug)
-        room = MessageRoom.objects.create(user=request.user, person=person)
-        room.save()
-    pass
+    return render(request, 'Messenger/messenger.html',
+                  {"room": room, "messages": messages, 'list_room': list_room, "profile": profile})
