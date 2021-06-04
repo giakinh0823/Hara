3295b4ac100caf749c899356d0c04b318230b495
Kính Hà Gia <giakinh2000@gmail.com>
2021-06-04 22:32:38 +0700

    validate user

--- Dealme/settings.py
+++ Dealme/settings.py
@@ -188,9 +188,9 @@ DATABASE_CONNECTION_POOLING = False
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
