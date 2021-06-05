3c4758cce393acbf05747ebe5e746d2b76d0368f
Kính Hà Gia <giakinh2000@gmail.com>
2021-06-05 20:14:26 +0700

    add send email when login

--- Dealme/myAdapter.py
+++ Dealme/myAdapter.py
@@ -1,15 +1,21 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.account.models import EmailAddress
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-
         user = sociallogin.user
         print(user)
         print(user.email)
         if user.id:
+            send_mail(
+                'Đăng nhập website Hara',
+                'Chúng tôi nhận thấy tài khoản của bạn đang hoạt động.',
+                'giakinhfullstack@gmail.com',
+                [str(user.email)],
+            )
             return
         if not user.email:
             return
